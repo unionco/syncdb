91eed3dbda8d0093dcbd26750e7a2d4361c2c8a0
Abry Rath <abry.rath@union.co>
2021-05-16 09:28:59 -0400

    logging

--- src/Service/Logger.php
+++ src/Service/Logger.php
@@ -33,22 +33,23 @@ class Logger
     public function __call($name, $args)
     {
         if ($this->ml) {
-            if (count($args) > 1 && $args[1]) {
-                $context = $args[1];
-                $context = \array_map(function ($val) {
-                    if (\is_object($val) || \is_array($val)) {
-                        return print_r($val, true);
-                    }
-                    return $val;
-                }, $context);
-                try {
-                    return $this->ml->{$name}($args[0], $context);
-                } catch (\Throwable $e) {
-                    $this->ml->error($e->getMessage());
-                }
+            // if (count($args) > 1 && $args[1]) {
+            //     $context = $args[1];
+            //     $context = \array_map(function ($val) {
+            //         if (\is_object($val) || \is_array($val)) {
+            //             return print_r($val, true);
+            //         }
+            //         return $val;
+            //     }, $context);
+            //     try {
+            //         return $this->ml->{$name}($args[0], $context);
+            //     } catch (\Throwable $e) {
+            //         $this->ml->error($e->getMessage());
+            //     }
+
             }
             try {
-                $this->ml->{$name}($args);
+                $this->ml->{$name}($args[0]);
             } catch (\Throwable $e) {
                 $this->ml->error($e->getMessage());
             }
